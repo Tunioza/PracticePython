00b63efe5745524ed81f6a8d72a7ead44299034a
Wiktor Zajda <zajda.wiktor1@gmail.com>
2024-01-31 22:22:24 +0100

    binary search recursion

--- 20_Element_Search.py
+++ 20_Element_Search.py
@@ -20,9 +20,36 @@ def find(ordered_list, element_to_find):
             start_index = middle_index
 
 
+def binary_search_recursion(ordered_list, element_to_search, *indeksy):
+    if len(indeksy) == 0:
+        start_index = 0
+        end_index = len(ordered_list) - 1
+    elif len(indeksy) == 2:
+        start_index = indeksy[0]
+        end_index = indeksy[1]
+    else:
+        return False
+
+    middle_index = (start_index + end_index) // 2
+
+    if middle_index < start_index or middle_index > end_index or middle_index < 0:
+        return False
+
+    middle_element = ordered_list[middle_index]
+
+    if middle_element == element_to_search:
+        return True
+    elif middle_element < element_to_search:
+        return binary_search_recursion(ordered_list, element_to_search, middle_index + 1, end_index)
+    else:
+        return binary_search_recursion(ordered_list, element_to_search, start_index, middle_index - 1)
+
+
+
 if __name__ == "__main__":
     l = [2, 4, 6, 8, 10]
-    print(find(l, 5))  # prints False
-    print(find(l, 10))  # prints True
-    print(find(l, (-1)))  # prints False
-    print(find(l, 2))  # prints True
+
+    print(binary_search_recursion(l, 6))  # prints False
+    print(binary_search_recursion(l, 5))  # prints False
+    print(binary_search_recursion(l, 8))  # prints False
+    print(binary_search_recursion(l, 1))  # prints False
